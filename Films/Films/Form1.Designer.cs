450ee64f3a8f8e31c3172b34d00139879406cf60
JuliaNaumets <julianaumets@github.com>
2015-04-17 17:10:45 +0300

    ObjectsProperty

--- Films/Films/Form1.Designer.cs
+++ Films/Films/Form1.Designer.cs
@@ -39,16 +39,18 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.labelEdit = new System.Windows.Forms.Label();
-            this.textBoxYear = new System.Windows.Forms.TextBox();
-            this.textBoxProducer = new System.Windows.Forms.TextBox();
-            this.textBoxDirector = new System.Windows.Forms.TextBox();
-            this.textBoxPath = new System.Windows.Forms.TextBox();
-            this.labelYear = new System.Windows.Forms.Label();
-            this.labelProducer = new System.Windows.Forms.Label();
-            this.labelDirector = new System.Windows.Forms.Label();
-            this.labelPath = new System.Windows.Forms.Label();
             this.buttonEdit = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.labelDirector = new System.Windows.Forms.Label();
+            this.labelProducer = new System.Windows.Forms.Label();
+            this.labelYear = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxDirector = new System.Windows.Forms.TextBox();
+            this.textBoxProducer = new System.Windows.Forms.TextBox();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.labelEdit = new System.Windows.Forms.Label();
+            this.objectsProperty = new System.Windows.Forms.Button();
+            this.informationBox = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -153,7 +155,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(418, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(498, 290);
+            this.pictureBox1.Size = new System.Drawing.Size(498, 230);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
@@ -174,79 +176,6 @@
             this.panel1.Size = new System.Drawing.Size(400, 170);
             this.panel1.TabIndex = 10;
             // 
-            // labelEdit
-            // 
-            this.labelEdit.AutoSize = true;
-            this.labelEdit.Location = new System.Drawing.Point(141, 16);
-            this.labelEdit.Name = "labelEdit";
-            this.labelEdit.Size = new System.Drawing.Size(91, 13);
-            this.labelEdit.TabIndex = 0;
-            this.labelEdit.Text = "Редактирование";
-            // 
-            // textBoxYear
-            // 
-            this.textBoxYear.Location = new System.Drawing.Point(5, 60);
-            this.textBoxYear.Name = "textBoxYear";
-            this.textBoxYear.Size = new System.Drawing.Size(50, 20);
-            this.textBoxYear.TabIndex = 1;
-            // 
-            // textBoxProducer
-            // 
-            this.textBoxProducer.Location = new System.Drawing.Point(61, 60);
-            this.textBoxProducer.Name = "textBoxProducer";
-            this.textBoxProducer.Size = new System.Drawing.Size(81, 20);
-            this.textBoxProducer.TabIndex = 2;
-            // 
-            // textBoxDirector
-            // 
-            this.textBoxDirector.Location = new System.Drawing.Point(148, 60);
-            this.textBoxDirector.Name = "textBoxDirector";
-            this.textBoxDirector.Size = new System.Drawing.Size(100, 20);
-            this.textBoxDirector.TabIndex = 3;
-            // 
-            // textBoxPath
-            // 
-            this.textBoxPath.Location = new System.Drawing.Point(254, 60);
-            this.textBoxPath.Name = "textBoxPath";
-            this.textBoxPath.Size = new System.Drawing.Size(143, 20);
-            this.textBoxPath.TabIndex = 4;
-            // 
-            // labelYear
-            // 
-            this.labelYear.AutoSize = true;
-            this.labelYear.Location = new System.Drawing.Point(16, 44);
-            this.labelYear.Name = "labelYear";
-            this.labelYear.Size = new System.Drawing.Size(24, 13);
-            this.labelYear.TabIndex = 5;
-            this.labelYear.Text = "год";
-            // 
-            // labelProducer
-            // 
-            this.labelProducer.AutoSize = true;
-            this.labelProducer.Location = new System.Drawing.Point(79, 44);
-            this.labelProducer.Name = "labelProducer";
-            this.labelProducer.Size = new System.Drawing.Size(63, 13);
-            this.labelProducer.TabIndex = 6;
-            this.labelProducer.Text = "продюссер";
-            // 
-            // labelDirector
-            // 
-            this.labelDirector.AutoSize = true;
-            this.labelDirector.Location = new System.Drawing.Point(160, 44);
-            this.labelDirector.Name = "labelDirector";
-            this.labelDirector.Size = new System.Drawing.Size(57, 13);
-            this.labelDirector.TabIndex = 7;
-            this.labelDirector.Text = "режиссёр";
-            // 
-            // labelPath
-            // 
-            this.labelPath.AutoSize = true;
-            this.labelPath.Location = new System.Drawing.Point(270, 44);
-            this.labelPath.Name = "labelPath";
-            this.labelPath.Size = new System.Drawing.Size(72, 13);
-            this.labelPath.TabIndex = 8;
-            this.labelPath.Text = "путь к файлу";
-            // 
             // buttonEdit
             // 
             this.buttonEdit.Location = new System.Drawing.Point(125, 128);
@@ -257,11 +186,104 @@
             this.buttonEdit.UseVisualStyleBackColor = true;
             this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
             // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(270, 44);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(72, 13);
+            this.labelPath.TabIndex = 8;
+            this.labelPath.Text = "путь к файлу";
+            // 
+            // labelDirector
+            // 
+            this.labelDirector.AutoSize = true;
+            this.labelDirector.Location = new System.Drawing.Point(160, 44);
+            this.labelDirector.Name = "labelDirector";
+            this.labelDirector.Size = new System.Drawing.Size(57, 13);
+            this.labelDirector.TabIndex = 7;
+            this.labelDirector.Text = "режиссёр";
+            // 
+            // labelProducer
+            // 
+            this.labelProducer.AutoSize = true;
+            this.labelProducer.Location = new System.Drawing.Point(79, 44);
+            this.labelProducer.Name = "labelProducer";
+            this.labelProducer.Size = new System.Drawing.Size(63, 13);
+            this.labelProducer.TabIndex = 6;
+            this.labelProducer.Text = "продюссер";
+            // 
+            // labelYear
+            // 
+            this.labelYear.AutoSize = true;
+            this.labelYear.Location = new System.Drawing.Point(16, 44);
+            this.labelYear.Name = "labelYear";
+            this.labelYear.Size = new System.Drawing.Size(24, 13);
+            this.labelYear.TabIndex = 5;
+            this.labelYear.Text = "год";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(254, 60);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(143, 20);
+            this.textBoxPath.TabIndex = 4;
+            // 
+            // textBoxDirector
+            // 
+            this.textBoxDirector.Location = new System.Drawing.Point(148, 60);
+            this.textBoxDirector.Name = "textBoxDirector";
+            this.textBoxDirector.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDirector.TabIndex = 3;
+            // 
+            // textBoxProducer
+            // 
+            this.textBoxProducer.Location = new System.Drawing.Point(61, 60);
+            this.textBoxProducer.Name = "textBoxProducer";
+            this.textBoxProducer.Size = new System.Drawing.Size(81, 20);
+            this.textBoxProducer.TabIndex = 2;
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(5, 60);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(50, 20);
+            this.textBoxYear.TabIndex = 1;
+            // 
+            // labelEdit
+            // 
+            this.labelEdit.AutoSize = true;
+            this.labelEdit.Location = new System.Drawing.Point(141, 16);
+            this.labelEdit.Name = "labelEdit";
+            this.labelEdit.Size = new System.Drawing.Size(91, 13);
+            this.labelEdit.TabIndex = 0;
+            this.labelEdit.Text = "Редактирование";
+            // 
+            // objectsProperty
+            // 
+            this.objectsProperty.Location = new System.Drawing.Point(815, 392);
+            this.objectsProperty.Name = "objectsProperty";
+            this.objectsProperty.Size = new System.Drawing.Size(101, 44);
+            this.objectsProperty.TabIndex = 11;
+            this.objectsProperty.Text = "Свойства всех обектов";
+            this.objectsProperty.UseVisualStyleBackColor = true;
+            this.objectsProperty.Click += new System.EventHandler(this.objectsProperty_Click);
+            // 
+            // informationBox
+            // 
+            this.informationBox.FormattingEnabled = true;
+            this.informationBox.Location = new System.Drawing.Point(418, 275);
+            this.informationBox.Name = "informationBox";
+            this.informationBox.Size = new System.Drawing.Size(391, 186);
+            this.informationBox.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(928, 457);
+            this.Controls.Add(this.informationBox);
+            this.Controls.Add(this.objectsProperty);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listBox1);
@@ -305,6 +327,8 @@
         private System.Windows.Forms.TextBox textBoxDirector;
         private System.Windows.Forms.TextBox textBoxProducer;
         private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Button objectsProperty;
+        private System.Windows.Forms.ListBox informationBox;
     }
 }
 
